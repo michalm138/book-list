ac899c1eb61395691d579e24e067d3b78fc51cb2
michalm138 <michalmichalak138@gmail.com>
2021-07-05 15:28:41 +0200

    Add book list view

--- book_list_app/urls.py
+++ book_list_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from book_list_app import views
 
 urlpatterns = [
-
+    path('app/book/list/', views.BookList.as_view(), name='book-list'),
 ]
--- book_list_app/views.py
+++ book_list_app/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from book_list_app import models
+from functools import reduce
+import operator
+from django.db.models import Q
+from datetime import datetime
 
-# Create your views here.
+
+class BookList(ListView):
+    template_name = 'list.html'
+    context_object_name = 'books'
+
+    def get_queryset(self):
+        data = self.request.GET
+        title = data.get('title')
+        author = data.get('author')
+        language = data.get('language')
+        pub_date_start = data.get('pub_date_start')
+        pub_date_end = data.get('pub_date_end')
+
+        query = {}
+
+        if title:
+            query['title__icontains'] = title
+        if author:
+            query['author__icontains'] = author
+        if language:
+            query['language__icontains'] = language
+        if pub_date_start and pub_date_end:
+            query['pub_date__range'] = (pub_date_start, pub_date_end)
+        elif pub_date_start and not pub_date_end:
+            query['pub_date__range'] = (pub_date_start, datetime.now().date())
+        elif not pub_date_start and pub_date_end:
+            query['pub_date__range'] = ('1970-01-01', pub_date_end)
+
+        if query:
+            response_data = models.Book.objects.filter(
+                reduce(operator.or_,
+                       (Q(q) for q in query.items()))
+            ).order_by('-id')
+        else:
+            response_data = models.Book.objects.all().order_by('-id')
+
+        return response_data
