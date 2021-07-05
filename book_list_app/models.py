da38a9f704ae6ac48c6d9a0509de1ade57f1ff45
michalm138 <michalmichalak138@gmail.com>
2021-07-05 12:19:36 +0200

    Add database object - Book

--- book_list_app/models.py
+++ book_list_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+import re
 
-# Create your models here.
+
+def isbn_validator(value):
+    if not re.search('^\d+$', value):
+        raise ValidationError('ISBN number should contains numbers only.')
+
+
+def pub_date_validator(value):
+    if not re.search('^\d{4}-\d{2}-\d{2}$|^\d{4}-\d{2}$|^\d{4}$', value):
+        raise ValidationError('Enter a valid date (ex. "YYYY", "YYYY-MM", "YYYY-MM-DD")')
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    pub_date = models.CharField(max_length=10, validators=[pub_date_validator])
+    isbn_number = models.CharField(max_length=13, validators=[isbn_validator])
+    page_count = models.PositiveIntegerField()
+    front_cover_link = models.URLField(blank=True, null=True)
+    language = models.CharField(max_length=45)
+
+    def __str__(self):
+        return f'{self.title} {self.author}'
